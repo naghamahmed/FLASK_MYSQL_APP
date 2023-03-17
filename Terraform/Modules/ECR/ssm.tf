61472e3fc5e620359c5800412a542641316e4b16
naghamahmed <naghammekhemar97@gmail.com>
2023-03-17 05:32:03 +0200

    Terraform

---
+++ Terraform/Modules/ECR/ssm.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "flask_image" {
+  name        = "/dev/flask-image"
+  description = "flask_image"
+  type        = "StringList"
+  value       = aws_ecr_repository.flask_repo.repository_url
+}
+
+resource "aws_ssm_parameter" "mysql_image" {
+  name        = "/dev/mysql_image"
+  description = "mysql_image"
+  type        = "StringList"
+  value       = aws_ecr_repository.mysql_repo.repository_url
+}
