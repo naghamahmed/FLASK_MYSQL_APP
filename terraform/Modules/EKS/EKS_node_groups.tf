d717f4bdb49d9062b903f78e6bee4d2437fde09b
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-03-20 19:37:01 +0200

    EKS

--- terraform/Modules/EKS/EKS_node_groups.tf
+++ terraform/Modules/EKS/EKS_node_groups.tf
@@ -49,11 +49,11 @@ resource "aws_eks_node_group" "nodes_general" {
   ]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
 
-    max_size = 1
+    max_size     = 2
 
-    min_size = 1
+    min_size     = 2
   }
 
   ami_type = "AL2_x86_64"
