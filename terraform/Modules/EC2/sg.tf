c6087d84622d786c715cd791f799fdf0f0d5103f
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-04-02 01:18:15 +0200

    Update sg.tf

--- terraform/Modules/EC2/sg.tf
+++ terraform/Modules/EC2/sg.tf
@@ -8,25 +8,11 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-    egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
     ingress {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-    egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
   }
 
     ingress {
